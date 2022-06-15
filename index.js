a21d5ea4f5a3cac13898a94d2fde09436e3c74f6
LifeSignals <venu@lifesignals.com>
2022-06-15 16:41:25 +0530

    init

--- index.js
+++ index.js
@@ -2,47 +2,58 @@ var api = require("./api")
 module.exports.init = async function (on, config, settings) {
     if (!settings) throw new Error('Settings not found');
     api.init(settings.token, settings.baseUrl);
-    on('after:run',async (results) => {
-        await api.AfterRun(results);
-    })
 
-    on('after:screenshot', async (details) => {
-        await api.AfterScreenshot(details);
-    })
+    // on('after:run',async (results) => {
+    //     results.project_id = settings.project_id;
+    //     await api.AfterRun(results);
+    // })
 
-    on('after:spec', async(spec, results) => {
-        var data = {
+    // on('after:screenshot', async (details) => {
+    //     details.project_id = settings.project_id;
+    //     await api.AfterScreenshot(details);
+    // })
+
+    // on('after:spec', async(spec, results) => {
+    //     var data = {
             
-        }
-        if(results){
-            data.results = results;
-        }
-        if(spec){
-            data.spec= spec;
-        }
-        await api.AfterSpec(data);
-    })
+    //     }
+    //     if(results){
+    //         data.results = results;
+    //     }
+    //     if(spec){
+    //         data.spec= spec;
+    //     }
+    //     data.project_id = settings.project_id;
+    //     await api.AfterSpec(data);
+    // })
 
     // on('before:browser:launch', async (browser = {}, launchOptions) => {
     //     var data = {
     //         browser : browser,
     //         launchOptions : launchOptions
     //     }
+    //     data.project_id = settings.project_id;
     //     await api.BeforeBrowserLaunch(data);
     // })
 
     on('before:run', async (details) => {
-        await api.BeforeRun(details);
+        try {
+            details.project_id = settings.project_id;
+            var d = await api.BeforeRun(details);
+            console.log(d)
+        } catch (error) {
+            console.log(error);
+        }
     })
 
-    on('before:spec', async (spec) => {
-        data = {
-            spec : spec,
+    // on('before:spec', async (spec) => {
+    //     data = {
+    //         spec : spec,
           
-        }
-        
-        await api.BeforeSpec(data);
-    })
+    //     }
+    //     data.project_id = settings.project_id;
+    //     await api.BeforeSpec(data);
+    // })
 
     // on('file:preprocessor', async (file) => {
     //     await api.FilePreprocessor(file)
